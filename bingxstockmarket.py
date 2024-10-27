99a8343780ee05bf29ad2466a5a9d51bb41598d7
Danil.Rutkovskiy <rutkovskiy.danil@mail.ru>
2024-10-27 05:39:26 +0500

    now it works

--- bingxstockmarket.py
+++ bingxstockmarket.py
@@ -7,15 +7,18 @@ class BingXStockMarketImpl(stockmarket.StockMarket):
     def __init__(self):
         self.url = "wss://open-api-ws.bingx.com/market"
         self.coinList = {}
+        self.threadCoinList = {}
 
     def createBtcCoin(self):
         channel = {"id": "e745cd6d-d0f6-4a70-8d5a-043e4c741b40", "reqType": "sub", "dataType": "BTC-USDT@lastPrice"}
         newCoin = bingxcoin.BingXCoinImpl(self.url, channel, "BING-X-BTC")
+        self.coinList["BTC"] = newCoin
         newCoin.start()
 
     def createEthCoin(self):
         channel = {"id": "e745cd6d-d0f6-4a70-8d5a-043e4c741b40", "reqType": "sub", "dataType": "ETH-USDT@lastPrice"}
         newCoin = bingxcoin.BingXCoinImpl(self.url, channel, "BING-X-ETX")
+        self.coinList["ETH"] = newCoin
         newCoin.start()
 
     def addCoin(self, name):
@@ -24,11 +27,11 @@ class BingXStockMarketImpl(stockmarket.StockMarket):
             thread_coin = threading.Thread(target=self.createBtcCoin, args=(), name="BTC")
         elif name == "ETH":
             thread_coin = threading.Thread(target=self.createEthCoin, args=(), name="ETH")
-        self.coinList[name] = thread_coin
+        self.threadCoinList[name] = thread_coin
         thread_coin.start()
 
     def start(self):
-        for coin_thread in self.coinList.values():
+        for coin_thread in self.threadCoinList.values():
             coin_thread.join()
 
     def getCoinCost(self, name):
--- main.py
+++ main.py
@@ -8,16 +8,16 @@ import time
 coins = {1:"BTC", 2:"ETH"}
 
 def testStock(stock):
-    stock.start()
     while True:
-        print(stock.getCoinCost("BTC"))
-        print(stock.getCoinCost("ETH"))
+        print("BTC: " + str(stock.getCoinCost("BTC")))
+        print("ETH: " + str(stock.getCoinCost("ETH")))
         time.sleep(1)
 
 if __name__ == "__main__":
     bingX = bingxstockmarket.BingXStockMarketImpl()
-
     bingX.addCoin("BTC")
     bingX.addCoin("ETH")
 
-    testStock(bingX)
+    thredd = threading.Thread(target=testStock, args=(bingX,), name="MainApplication")
+    thredd.start()
+    bingX.start()
